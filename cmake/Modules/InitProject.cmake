0065f4284261cb0baa4cc3a8ddbefc950f72348a
Sebastian Ziebell <sebastian.ziebell@asquera.de>
2012-06-19 17:05:13 +0200

    a cmake file detects the appropriate Mac OSX version when an apple platform is detected

--- cmake/Modules/InitProject.cmake
+++ cmake/Modules/InitProject.cmake
@@ -16,7 +16,9 @@ elseif (APPLE)
     set (CMAKE_CXX_FLAGS_RELEASE "-O3")
     set (CMAKE_C_FLAGS_DEBUG "-g -Wall")
     set (CMAKE_C_FLAGS_RELEASE "-O3")
- 
+
+    include (MacOSXVersion)
+
 else ()
     add_definitions (-DLINUX -DUNIX)
     set (LINUX TRUE)
