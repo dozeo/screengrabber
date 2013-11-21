9ad91fe2013fde8e5eda84368f13bcd94e20a1b7
Konstantinos Sofokleous <kostas@dozeo.com>
2013-11-21 15:58:12 +0100

    fixed PATH to be updated after discovery of VS path

--- build_dependencies_win.sh
+++ build_dependencies_win.sh
@@ -30,26 +30,30 @@ echo "INSTALL_DIR: $INSTALL_DIR"
 # download by hand
 # git version doesn't work, as no autoconf is installed by default
 if [ -e $INSTALL_DIR/bin/yasm ]; then
-    echo "yasm seems to already exist"
+	echo "yasm seems to already exist"
 else
-    if [ -d $SRC_DIR/yasm-1.2.0 ]; then
-        echo "Yasm already downloaded"
-    else
+	if [ -d $SRC_DIR/yasm-1.2.0 ]; then
+		echo "Yasm already downloaded"
+	else
 		(cd $SRC_DIR && wget --no-check-certificate https://github.com/downloads/dozeo/screengrabber/yasm-1.2.0.tar.gz -O yasm-1.2.0.tar.gz && tar zxvf yasm-1.2.0.tar.gz && rm yasm-1.2.0.tar.gz)
-    fi
+	fi
 	
-    echo "Compiling yasm ..."
+	echo "Compiling yasm ..."
 
-    (cd $SRC_DIR/yasm-1.2.0 && ./configure --prefix=$INSTALL_DIR && make install)
+	(
+		cd $SRC_DIR/yasm-1.2.0
+		./configure --prefix=$INSTALL_DIR
+		make install
+	)
 fi
 
 
 # ffmpeg requires pkg-config
 # =================================================================
 if [ -e $INSTALL_DIR/bin/pkg-config.exe ]; then
-    echo "pkgconfig seems to already exist"
+	echo "pkgconfig seems to already exist"
 else
-    echo "Fetching pkgconfig"
+	echo "Fetching pkgconfig"
 	
 	(
 		PKG_FILENAME=pkg-config_0.28-1_bin-win32.zip
@@ -69,10 +73,10 @@ fi
 # compile polarssl
 # =================================================================
 if [ -e $INSTALL_DIR/lib/polarssl.dll ]; then
-    echo "polarssl seems to already exist"
+	echo "polarssl seems to already exist"
 else
 
-    (
+	(
 		cd $SRC_DIR/polarssl
 		make SYS=posix DESTDIR=$INSTALL_DIR lib
 		make SYS=posix DESTDIR=$INSTALL_DIR install
@@ -85,15 +89,15 @@ fi
 # rtmpdump
 # =================================================================
 if [ -e $INSTALL_DIR/bin/rtmpdump ]; then
-    echo "rtmpdump seems to already exist"
+	echo "rtmpdump seems to already exist"
 else
-    echo "Compiling rtmpdump ..."
+	echo "Compiling rtmpdump ..."
 	
 	(
 		cd $SRC_DIR/rtmpdump
 		export LIBZ="-lssl -lcrypto -ldl" 
 		make CRYPTO=POLARSSL SYS=mingw prefix=$INSTALL_DIR XCFLAGS=-I$INSTALL_DIR/include XLDFLAGS=-L$INSTALL_DIR/lib install
-    )
+	)
 fi
 # =================================================================
 
@@ -122,7 +126,7 @@ GTESTNAME=gtest-1.6.0
 GTESTPATH=$SRC_DIR
 GTESTURL=http://googletest.googlecode.com/files/gtest-1.6.0.zip
 if [ -d $SRC_DIR/$GTESTNAME ]; then
-    echo "gtest seems already downloaded"
+	echo "gtest seems already downloaded"
 else
 	(
 		cd $SRC_DIR
@@ -137,24 +141,23 @@ fi
 # lib x264 needs to get build!
 # =================================================================
 if [ -e $INSTALL_DIR/bin/x264 ]; then
-    echo "libx264 seems to already exist"
+	echo "libx264 seems to already exist"
 else
-    echo "Compiling x264 ..."
+	echo "Compiling x264 ..."
 
-    (
-        cd $SRC_DIR/x264
-        ./configure --enable-shared --prefix=$INSTALL_DIR
-        #--enable-debug --disable-asm --enable-win32thread
-        make install
-    )
-    #make install
+	(
+		cd $SRC_DIR/x264
+		./configure --enable-shared --prefix=$INSTALL_DIR
+		#--enable-debug --disable-asm --enable-win32thread
+		make install
+	)
+	#make install
 fi
 # =================================================================
 
 
 # enable VSDIR here
 # =================================================================
-export PATH=$PATH:$PATH:"$VSDIR/VC/bin":"$VSDIR/Common7/IDE"
 # HACK determine installed Visual Studio, can differ for OS, 32 / 64 Bit
 if [ -d "/C/Program Files/Microsoft Visual Studio 9.0/VC" ]; then
 	echo "Using Visual Studio 2008"
@@ -172,6 +175,7 @@ else
 	echo "Fatal: No visual studio found, ffmpeg won't link correctly without lib.exe"
 	exit 1
 fi
+export PATH=$PATH:$PATH:"$VSDIR/VC/bin":"$VSDIR/Common7/IDE"
 echo "VSDIR: $VSDIR"
 # =================================================================
 
