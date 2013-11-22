d3951477a16e168e7831d6e094a6918b2d2ef67b
Konstantinos Sofokleous <kostas@dozeo.com>
2013-11-22 16:07:35 +0100

    fix small problem of not terminating immediately when a tool was missing

--- build_dependencies_win.sh
+++ build_dependencies_win.sh
@@ -4,12 +4,22 @@ ABSDIR=`(cd $(dirname $0); pwd)`
 
 # Fail on errors
 set -e
+set -o pipefail
 
 # define directories
 SRC_DIR=$ABSDIR/dependencies_source
 INSTALL_DIR=$ABSDIR/dependencies
 
-export PATH=/C/MinGW/bin:$PATH
+if [ -d /mingw ]; then
+	echo "Mingw found in /mingw using that"
+	export PATH=/mingw:$PATH
+fi
+
+if [ -d /C/MinGW/bin ]; then
+	echo "Mingw found in C:\MinGW"
+	export PATH=/C/MinGW/bin:$PATH
+fi
+
 export PATH=$PATH:$INSTALL_DIR/bin
 export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:$INSTALL_DIR/lib/pkgconfig
 
@@ -35,14 +45,20 @@ else
 	if [ -d $SRC_DIR/yasm-1.2.0 ]; then
 		echo "Yasm already downloaded"
 	else
-		(cd $SRC_DIR && wget --no-check-certificate https://github.com/downloads/dozeo/screengrabber/yasm-1.2.0.tar.gz -O yasm-1.2.0.tar.gz && tar zxvf yasm-1.2.0.tar.gz && rm yasm-1.2.0.tar.gz)
+		(
+			echo "Downloading Yasm" &&
+			cd $SRC_DIR &&
+			wget --no-check-certificate https://github.com/downloads/dozeo/screengrabber/yasm-1.2.0.tar.gz -O yasm-1.2.0.tar.gz &&
+			tar zxvf yasm-1.2.0.tar.gz &&
+			rm yasm-1.2.0.tar.gz
+		) || exit 1;
 	fi
 	
 	echo "Compiling yasm ..."
 
 	(
-		cd $SRC_DIR/yasm-1.2.0
-		./configure --prefix=$INSTALL_DIR
+		cd $SRC_DIR/yasm-1.2.0 &&
+		./configure --prefix=$INSTALL_DIR &&
 		make install
 	)
 fi
@@ -59,28 +75,28 @@ else
 		PKG_FILENAME=pkg-config_0.28-1_bin-win32.zip
 		PKG_URL=http://kent.dl.sourceforge.net/project/pkgconfiglite/0.28-1/pkg-config-lite-0.28-1_bin-win32.zip
 
-		cd $INSTALL_DIR/
+		cd $INSTALL_DIR/ &&
 
-		wget $PKG_URL -O $PKG_FILENAME
-		unzip -o $PKG_FILENAME
-		cp -rf pkg-config-lite-0.28-1/* .
+		wget $PKG_URL -O $PKG_FILENAME &&
+		unzip -o $PKG_FILENAME &&
+		cp -rf pkg-config-lite-0.28-1/* . &&
 		rm -rf pkg-config-lite-0.28-1/ $PKG_FILENAME
-	)
+	) || exit 1;
 fi
 # =================================================================
 
 
 # compile polarssl
 # =================================================================
-if [ -e $INSTALL_DIR/lib/polarssl.dll ]; then
+if [ -e $INSTALL_DIR/lib/libpolarssl.a ]; then
 	echo "polarssl seems to already exist"
 else
 
 	(
-		cd $SRC_DIR/polarssl
-		make SYS=posix DESTDIR=$INSTALL_DIR lib
+		cd $SRC_DIR/polarssl &&
+		make SYS=posix DESTDIR=$INSTALL_DIR lib &&
 		make SYS=posix DESTDIR=$INSTALL_DIR install
-	)
+	) || exit 1;
 		
 fi
 # =================================================================
@@ -94,10 +110,10 @@ else
 	echo "Compiling rtmpdump ..."
 	
 	(
-		cd $SRC_DIR/rtmpdump
-		export LIBZ="-lssl -lcrypto -ldl" 
+		cd $SRC_DIR/rtmpdump &&
+		export LIBZ="-lssl -lcrypto -ldl" &&
 		make CRYPTO=POLARSSL SYS=mingw prefix=$INSTALL_DIR XCFLAGS=-I$INSTALL_DIR/include XLDFLAGS=-L$INSTALL_DIR/lib install
-	)
+	) || exit 1;
 fi
 # =================================================================
 
@@ -110,12 +126,12 @@ ZLIBPATH=$INSTALL_DIR/bin/$ZLIBFILE
 if [ -e $ZLIBPATH ]; then
 	echo "$ZLIBFILE exists"
 else
-	echo "Compiling $ZLIBFILE"
-	
-	cd $SRC_DIR/zlib
-	
-	make -f win32/Makefile.gcc
-	cp -rf zlib1.dll $ZLIBPATH
+	echo "Building $ZLIBFILE"
+	(
+		cd $SRC_DIR/zlib &&
+		make -f win32/Makefile.gcc &&
+		cp -rf zlib1.dll $ZLIBPATH
+	) || exit 1;
 fi
 # =================================================================
 
@@ -129,11 +145,11 @@ if [ -d $SRC_DIR/$GTESTNAME ]; then
 	echo "gtest seems already downloaded"
 else
 	(
-		cd $SRC_DIR
-		wget $GTESTURL -O $GTESTNAME.zip
-		unzip $GTESTNAME.zip
+		cd $SRC_DIR &&
+		wget $GTESTURL -O $GTESTNAME.zip &&
+		unzip $GTESTNAME.zip &&
 		rm $GTESTNAME.zip
-	)
+	) || exit 1;
 fi
 # =================================================================
 
@@ -146,8 +162,8 @@ else
 	echo "Compiling x264 ..."
 
 	(
-		cd $SRC_DIR/x264
-		./configure --enable-shared --prefix=$INSTALL_DIR
+		cd $SRC_DIR/x264 &&
+		./configure --enable-shared --prefix=$INSTALL_DIR &&
 		#--enable-debug --disable-asm --enable-win32thread
 		make install
 	)
@@ -188,13 +204,13 @@ else
 	echo "Compiling ffmpeg ..."
 	
 	(
-		cd $SRC_DIR/ffmpeg
+		cd $SRC_DIR/ffmpeg &&
 
-		./configure --prefix=$INSTALL_DIR --enable-shared --enable-libx264 --enable-gpl --enable-librtmp --enable-encoder=libx264 --enable-protocol=rtmp --enable-protocol=rtmps --enable-protocol=rtmpts --enable-protocol=rtmpte --enable-protocol=tcp --enable-protocol=file
+		./configure --prefix=$INSTALL_DIR --enable-shared --enable-libx264 --enable-gpl --enable-librtmp --enable-encoder=libx264 --enable-protocol=rtmp --enable-protocol=rtmps --enable-protocol=rtmpts --enable-protocol=rtmpte --enable-protocol=tcp --enable-protocol=file &&
 		
-		make
+		make &&
 		make install -k
-	)
+	) || exit 1;
 	
 fi
 # =================================================================
@@ -206,14 +222,13 @@ fi
 #function 
 CheckDLL() {
 	if [ -e $INSTALL_DIR/bin/$1 ]; then
-		echo "$2 ($1) already exists."
+		echo "$2 ($1) skipping copy... already exists."
 	else
 		echo "Copy $2 ($1)"
 		export DLL_PLACE=`which $1`
-		cp $DLL_PLACE $INSTALL_DIR/bin
+		cp $DLL_PLACE $INSTALL_DIR/bin || exit 1;
 	fi
 }
 
-#CheckDLL libz-1.dll "Zlib DLL"
 CheckDLL libgcc_s_dw2-1.dll "GCC Support Library"
 CheckDLL pthreadGC2.dll "Pthread"
