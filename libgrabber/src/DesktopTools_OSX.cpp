e09e1104357e56e698f865b6a25f7ae3460ccb49
Konstantinos Sofokleous <kostas@dozeo.com>
2014-02-26 09:45:55 +0100

    created the desktop tools for OSX

--- libgrabber/src/DesktopTools_OSX.cpp
+++ libgrabber/src/DesktopTools_OSX.cpp
@@ -1,6 +1,53 @@
 #ifdef MAC_OSX
 
-#include "DesktopTools.h"
+#include "DesktopTools_OSX.h"
+
+namespace dz
+{
+	DesktopTools_OSX::DesktopTools_OSX()
+	{
+		const uint32_t max = sizeof(mDisplays) / sizeof (CGDirectDisplayID);
+
+		/*CGGetOnlineDisplayList would also return sleeping and mirrored displays*/ 
+		CGGetActiveDisplayList(max, mDisplays, &mDisplayCount);
+		for (uint32_t i = 0; i < mDisplayCount; i++)
+		{
+			CGRect bounds = CGDisplayBounds(mDisplays[i]);
+			mDisplaySizes[i] = Rect(bounds.origin.x, bounds.origin.y, bounds.size.width, bounds.size.height);
+		}
+	}
+
+	DesktopTools_OSX::~DesktopTools_OSX()
+	{
+	}
+
+	uint32_t DesktopTools_OSX::GetScreenCount() const
+	{
+		return mDisplayCount;
+	}
+
+	Rect IDesktopTools::GetScreenResolution(uint32_t screen) const
+	{
+		if (screen >= mDisplayCount)
+			return dz::Rect(); // invalid screen
+		return mDisplaySizes[screen];
+	}
+
+	Rect IDesktopTools::GetCombinedScreenResolution() const
+	{
+		if (mDisplayCount == 0)
+			return Rect();
+		
+		if (mDisplayCount == 1)
+			return mDisplaySizes[0];
+
+		Rect r (mDisplaySizes[0]);
+		for (int i = 1; i < mDisplayCount; i++)
+			r.addToBoundingRect(mDisplaySizes[i]);
+		
+		return r;
+	}
+}
 
 
 #endif // MAC_OSX
---
+++ libgrabber/src/DesktopTools_OSX.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#ifdef MAC_OSX
+
+#include "IDesktopTools.h"
+#include "Grabber.h"
+
+namespace dz
+{
+	class DesktopTools_OSX : public IDesktopTools
+	{
+		public:
+			DesktopTools_OSX();
+			virtual ~DesktopTools_OSX();
+
+			virtual uint32_t GetScreenCount() const;
+			virtual Rect GetScreenResolution(uint32_t screen) const;
+			virtual Rect GetCombinedScreenResolution() const;
+
+		private:
+			std::vector<Display> m_displays;
+
+			// number of connected displays
+			uint32_t mDisplayCount;
+
+			// connected displays
+			CGDirectDisplayID mDisplays[16];
+
+			Rect mDisplaySizes[16];
+
+	};
+}
+
+#endif // MAC_OSX
+
+// EOF
